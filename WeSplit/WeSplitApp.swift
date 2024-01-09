df509886a6ecd4a884a2d7e3f6a5124820022d8d
Jay Park <jaybinpark@gmail.com>
2024-01-09 12:47:05 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jay Park on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
