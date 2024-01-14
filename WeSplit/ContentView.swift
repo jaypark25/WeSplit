5870d586bccb03a2207ca91c6085b1c26ad15157
Jay Park <jaybinpark@gmail.com>
2024-01-13 17:24:50 -0800

    Final version

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,20 +5,4 @@
 //  Created by Jay Park on 2024-01-09.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
+import Foundation
